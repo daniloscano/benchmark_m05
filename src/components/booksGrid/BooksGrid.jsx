5c1f50531c4113cb9e766cbe07bd670fe0f805de
Danilo Scano <daniloscano@gmail.com>
2025-05-11 13:00:34 +0200

    added base style to book card; to add palette

--- src/components/booksGrid/BooksGrid.jsx
+++ src/components/booksGrid/BooksGrid.jsx
@@ -13,8 +13,8 @@ const BooksGrid = () => {
         <>
             <section id="all-books">
                 <div className="container-fluid p-3">
-                    <h1>Books</h1>
-                    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4">
+                    <h1 className="mt-3">Books</h1>
+                    <div className="row row-cols-1 row-cols-md-3 row-cols-lg-5 gy-4 mt-2">
                         {
                             !isLoading && !error && books && (
                                 books.map((book, index) => (
--- src/components/booksGrid/partials/BookCard.jsx
+++ src/components/booksGrid/partials/BookCard.jsx
@@ -1,8 +1,8 @@
 import './bookCard.css'
 import {useState} from "react";
 
-const BookCard = ({ book }) => {
-    const [ isHover, setIsHover ] = useState(false)
+const BookCard = ({book}) => {
+    const [isHover, setIsHover] = useState(false)
 
     const hoverHandler = () => {
         setIsHover(prev => !prev)
@@ -16,20 +16,24 @@ const BookCard = ({ book }) => {
                 className={`book-card rounded rounded-2 ${isHover ? 'hovered' : ''}`}
             >
                 <img
-                    className="img-fluid"
+                    className="card-image"
                     src={book.img}
                     alt={book.title}
                 />
-                <div className="card-title px-2">
+                <div className="d-flex flex-column justify-content-end gap-2 card-details p-2 py-4">
                     <p
-                        className="fw-bold"
+                        className="fs-5 fw-bold truncate-2-lines"
                     >
                         {book.title}
                     </p>
-                </div>
-                <div className="card-details px-2">
-                    <small>{book.category}</small>
-                    <p>{book.price} €</p>
+                    <div className="d-flex justify-content-between align-items-center">
+                        <p className="m-0">{book.category}</p>
+                        <p
+                            className="text-end fw-bold m-0"
+                        >
+                            {book.price} €
+                        </p>
+                    </div>
                 </div>
             </div>
         </>
