1896218beef3369b5d45e8d7ddd0926f08b84441
Danilo Scano <daniloscano@gmail.com>
2025-05-13 19:53:27 +0200

    completed userComment component

--- src/components/commentsSection/partials/UserComment.jsx
+++ src/components/commentsSection/partials/UserComment.jsx
@@ -1,7 +1,31 @@
 import {Rating} from "primereact/rating";
 import './userComment.css'
+import {useEffect, useRef, useState} from "react";
 
 const UserComment = ({comment}) => {
+    const [onEdit, setOnEdit] = useState(false)
+    const [text, setText] = useState(comment.comment)
+    const [rating, setRating] = useState(comment.rate)
+
+    const inputRef = useRef(null)
+
+    const editHandler = () => {
+        setOnEdit(prev => !prev)
+    }
+
+    const commentChange = (e) => {
+        setText(e.target.value)
+    }
+
+    const ratingChange = (e) => {
+        setRating(e.target.value)
+    }
+
+    useEffect(() => {
+        if (onEdit && inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, [onEdit]);
 
     return (
         <>
@@ -13,20 +37,39 @@ const UserComment = ({comment}) => {
                 </div>
                 <div className="col col-8">
                     <div className="d-flex flex-column align-items-start gap-2 py-2 comment-content">
-                        <p className="mb-0 comment-text">
-                            {comment.comment}
-                        </p>
+                        {
+                            onEdit && (
+                                <input
+                                    name="comment-text"
+                                    id="comment-text-input"
+                                    onChange={commentChange}
+                                    ref={inputRef}
+                                    value={text}
+                                    type="text"/>
+                            )
+                        }
+                        {
+                            !onEdit && (
+                                <p className="mb-0 comment-text">
+                                    {text}
+                                </p>
+                            )
+                        }
                         <div className="d-flex justify-content-center comment-rating">
                             <Rating
-                                value={comment.rate}
-                                readOnly
+                                value={rating}
+                                readOnly={!onEdit}
+                                onChange={ratingChange}
                                 cancel={false}
                             />
                         </div>
                     </div>
                 </div>
                 <div className="col col-2 d-flex justify-content-start gap-2">
-                    <button className="btn rounded rounded-2 edit-btn">
+                    <button
+                        onClick={editHandler}
+                        className="btn rounded rounded-2 edit-btn"
+                    >
                         E
                     </button>
                     <button className="btn rounded rounded-2 delete-btn">
