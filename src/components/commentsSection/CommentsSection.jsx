b1fa6b9fb2c26e2fc4137049b42c3ba6ba1543f9
Danilo Scano <daniloscano@gmail.com>
2025-05-14 02:38:13 +0200

    minor fix: added margin bottom to commentsSection component

--- src/components/commentsSection/CommentsSection.jsx
+++ src/components/commentsSection/CommentsSection.jsx
@@ -4,7 +4,7 @@ import CommentForm from "./partials/CommentForm.jsx";
 const CommentsSection = () => {
     return (
         <>
-            <div className="container w-75 mx-auto pt-3 mt-3" id="comments-section">
+            <div className="container w-75 mx-auto py-3 my-3" id="comments-section">
                 <CommentsList />
                 <CommentForm />
             </div>
