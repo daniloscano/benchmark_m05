a42af6460182da5824cbafb835d61f9d828a92e8
Danilo Scano <daniloscano@gmail.com>
2025-05-18 14:38:33 +0200

    fixed HomePage test with new MobileMenuContext

--- src/pages/HomePage.test.jsx
+++ src/pages/HomePage.test.jsx
@@ -1,20 +1,23 @@
-import { render, screen } from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
 import HomePage from "./HomePage.jsx";
 import {MemoryRouter} from "react-router-dom";
 import {ThemeProvider} from "../contexts/ThemeContext.jsx";
 import {BooksProvider} from "../contexts/BooksContext.jsx";
 import {CommentsProvider} from "../contexts/CommentsContext.jsx";
+import {MobileMenuProvider} from "../contexts/MobileMenuContext.jsx";
 
 describe('Test for Home page', () => {
     it('should not render UserComment component', () => {
         render(
             <MemoryRouter>
                 <ThemeProvider>
-                    <BooksProvider>
-                        <CommentsProvider>
-                            <HomePage />
-                        </CommentsProvider>
-                    </BooksProvider>
+                    <MobileMenuProvider>
+                        <BooksProvider>
+                            <CommentsProvider>
+                                <HomePage/>
+                            </CommentsProvider>
+                        </BooksProvider>
+                    </MobileMenuProvider>
                 </ThemeProvider>
             </MemoryRouter>)
 
